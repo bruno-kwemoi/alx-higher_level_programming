6ad6f4f711d24f30d7dc36dcef2beda77c911dcc
bruno-kwemoi <bsaekwo@gmail.com>
2021-11-22 14:39:13 +0300

    corrected list range

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 def element_at(my_list, idx):
     n = len(my_list)
     if idx < 0:
-        return(None)
-    if idx > n:
-        return(None)
+        return (None)
+    if idx > n - 1:
+        return (None)
     else:
         return(my_list[idx])
