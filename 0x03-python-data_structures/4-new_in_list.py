8895e2bf5ac78f672dec5d484f7735e6f0bca18a
bruno-kwemoi <bsaekwo@gmail.com>
2021-11-22 15:47:00 +0300

    corrected list range

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     if idx < 0:
         return(new_list)
-    if idx > n:
+    if idx > n - 1:
         return(new_list)
     else:
         new_list[idx] = element
