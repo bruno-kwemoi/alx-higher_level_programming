d2a519dab5f379c12a34e4d6ecc5c8429eea3bb9
bruno-kwemoi <bsaekwo@gmail.com>
2021-11-22 14:41:35 +0300

    corrected list range

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     n = len(my_list)
     if idx < 0:
         return(my_list)
-    if idx > n:
+    if idx > n - 1:
         return(my_list)
     else:
         my_list[idx] = element
