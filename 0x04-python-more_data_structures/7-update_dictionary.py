3dcbf2c01c53bafb07e6674d57f2ac0633ded883
bruno-kwemoi <bsaekwo@gmail.com>
2021-11-24 23:09:44 +0300

    correct output

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dict = {key : value}
-    a_dictionary.update(new_dict)
-    return(a_dictionary)
+    if a_dictionary is not None:
+        new_dict = {key : value}
+        a_dictionary.update(new_dict)
+        return(a_dictionary)
+    return None
