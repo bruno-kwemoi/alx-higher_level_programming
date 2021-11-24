cd180ee32dff29a35b754ff853bc1509a9120aac
bruno-kwemoi <bsaekwo@gmail.com>
2021-11-24 23:05:15 +0300

    correct output

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update(a_dictionary[key] = value)
+    new_dict = {key : value}
+    a_dictionary.update(new_dict)
     return(a_dictionary)
