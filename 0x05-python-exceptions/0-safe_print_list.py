a9b28fd8bbce69ba8ebedc5c60f193629876e84d
bruno-kwemoi <bsaekwo@gmail.com>
2021-12-06 19:57:53 +0300

    corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     idx = 0
-    for i in range(x):
+    while idx < x:
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{}".format(my_list[idx]), end="")
         except IndexError:
             break
         idx += 1
