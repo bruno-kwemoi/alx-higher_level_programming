fe4cc7b0e4e50ad791ccdbdd17648761a2eb5ea2
bruno-kwemoi <bsaekwo@gmail.com>
2021-12-08 13:02:49 +0300

    first commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 1-square.py
+class Square
+"""
+
+
+class Square:
+    """
+    defines a square
+    args: self, size
+    """
+    def __init__(self, size):
+        self.__size = size
