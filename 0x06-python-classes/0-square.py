29432c27b8496f40485643e337e99774dbe7c09c
bruno-kwemoi <bsaekwo@gmail.com>
2021-12-08 12:34:59 +0300

    pycodestyle

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
 Module 0-square.py
+
 class Square
 """
-class Square:
-    """It defines class square.
 
+
+class Square:
+    """
+    It defines class square.
     does nothing.
     """
     pass
