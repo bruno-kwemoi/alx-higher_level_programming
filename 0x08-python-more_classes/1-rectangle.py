4e07b95bfa39b07567c00a70ce272a29a490a069
bruno-kwemoi <bsaekwo@gmail.com>
2022-02-01 04:58:27 +0300

    first commit

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+module 1-rectangle
+"""
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrives width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the value of width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """retrives height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the value of height"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
