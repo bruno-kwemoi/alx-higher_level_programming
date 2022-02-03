4c166f9cc3a641c08260175a3e76b45b6ca4b4f2
bruno-kwemoi <bsaekwo@gmail.com>
2022-02-03 05:40:25 +0300

    updates

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- creates the table on your MYSQL server
-CREATE TABLE IF NOT EXISTS force_name (
+CREATE TABLE IF NOT EXISTS force_name(
 id INT,
-name VARCHAR(256) NOT NULL;
-)
+name VARCHAR(256) NOT NULL
+);
